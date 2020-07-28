1a4cb0d0fbe5517d546f8c7e767fdb70a24a897a
duaimei <adetwiler@optoro.com>
2020-07-28 17:35:59 -0400

    this works nominally

---
+++ app/components/Next.vue
@@ -0,0 +1,16 @@
+<template>
+    <Page class="page">
+        <Label text="Second" />
+    </Page>
+</template>
+
+<script>
+    export default {
+        created() {
+            console.log('Second page was entered')
+        }
+    };
+</script>
+
+<style>
+</style>
--- app/main.js
+++ app/main.js
@@ -1,12 +1,13 @@
 import Vue from 'nativescript-vue'
 import App from './components/App'
-import VueDevtools from 'nativescript-vue-devtools'
+// import VueDevtools from 'nativescript-vue-devtools'
+// import { AR } from 'nativescript-ar'
 
-if(TNS_ENV !== 'production') {
-  Vue.use(VueDevtools)
-}
-import store from './store'
-
+// if(TNS_ENV !== 'production') {
+//   Vue.use(VueDevtools)
+// }
+import {store} from './store'
+// Vue.registerElement("AR", () => require("nativescript-ar").AR)
 // Prints Vue logs when --env.production is *NOT* set while building
 Vue.config.silent = (TNS_ENV === 'production')
 
